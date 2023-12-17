7551eecef8dd1ecb1f9cbbd968b4cc5fb8bab376
chaknogf <chaknogf@gmail.com>
2023-12-17 17:38:34 -0600

    Commit de los cambios pendientes

---
+++ src/app/estadistica/report_estd/report_estd.component.ts
@@ -0,0 +1,90 @@
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ReportsService } from 'src/app/services/reports.service';
+
+
+@Component({
+  selector: 'app-report_estd',
+  templateUrl: './report_estd.component.html',
+  styleUrls: ['./report_estd.component.css']
+})
+export class Report_estdComponent implements OnInit {
+
+  public opcion: number = 0;
+  public fecha_inicio_consultas: string = "";
+  public fecha_final_consultas: string = "";
+  public fecha_inicio_uisau: string = "";
+  public fecha_final_uisau: string = "";
+  cargando: boolean = false;
+
+  constructor(
+    private rp: ReportsService,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  descargarConsultas() {
+    this.rp.excel_consultas(this.fecha_inicio_consultas, this.fecha_final_consultas).subscribe(
+      (response: any) => {
+        const blob = new Blob([response.body], { type: 'application/octet-stream' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = 'consultas.xlsx';
+        a.click();
+        window.URL.revokeObjectURL(url);
+      },
+      error => {
+        console.error('Error al descargar el archivo', error);
+        // Manejar el error según sea necesario
+      }
+    );
+  }
+
+  descargarUisau() {
+    this.rp.excel_uisau(this.fecha_inicio_uisau, this.fecha_final_uisau).subscribe(
+      (response: any) => {
+        const blob = new Blob([response.body], { type: 'application/octet-stream' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = 'uisau.xlsx';
+        a.click();
+        window.URL.revokeObjectURL(url);
+      },
+      error => {
+        console.error('Error al descargar el archivo', error);
+        // Manejar el error según sea necesario
+      }
+    );
+  }
+
+  descargarPacientes() {
+    this.cargando = true; // Activar el loader
+    this.rp.excel_pacientes().subscribe(
+      (response: HttpResponse<Blob>) => {
+        this.descargarArchivo(response.body, 'pacientes.xlsx');
+        this.cargando = false; // Desactivar el loader después de la descarga
+      },
+      error => {
+        console.error('Error al descargar el archivo', error);
+        this.cargando = false; // Desactivar el loader en caso de error
+        // Manejar el error según sea necesario
+      }
+    );
+  }
+
+  descargarArchivo(data: any, nombreArchivo: string) {
+    const blob = new Blob([data], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    a.href = url;
+    a.download = nombreArchivo;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+}
---
+++ src/app/services/reports.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable, interval } from 'rxjs';
+import { environment } from 'src/enviroments/enviroment';
+import { UsersService } from './user.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportsService {
+
+  private token = this.auth.getTokenLocally(); // Obtén el token almacenado
+  private urlapi = environment.apiUrl;
+  constructor(
+    private http: HttpClient,
+    private auth: UsersService
+  ) { }
+
+  excel_consultas(inicio: string, final: string): Observable<HttpResponse<Blob>> {
+    const url = `${this.urlapi}/report_consult/?fecha_inicio=${inicio}&fecha_final=${final}&token=${this.token}`;
+    return this.http.get(url, { observe: 'response', responseType: 'blob' });
+  }
+
+
+  excel_uisau(inicio: string, final: string): Observable<any> {
+    const url = `${this.urlapi}/report_uisau/?fecha_inicio=${inicio}&fecha_final=${final}&token=${this.token}`;
+    return this.http.get(url, { observe: 'response', responseType: 'blob' });
+  }
+
+  excel_pacientes(): Observable<any> {
+    const url = `${this.urlapi}/report_paciente/?token=${this.token}`;
+    return this.http.get(url, { observe: 'response', responseType: 'blob' });
+  }
+
+}
