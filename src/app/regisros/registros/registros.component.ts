e25cf85261b75f88eff720b996dc232a8fbd43ba
chaknogf <chaknogf@gmail.com>
2024-02-18 11:39:19 -0600

    navbar

--- src/app/regisros/registros/registros.component.ts
+++ src/app/regisros/registros/registros.component.ts
@@ -10,6 +10,20 @@ export class RegistrosComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.onBlur()
+  }
+
+  mostrarRegistros = false;
+
+  onFocus() {
+    this.mostrarRegistros = true;
+  }
+
+  onBlur() {
+    // Añade un pequeño retardo para asegurar que el usuario no está cambiando el foco a otro elemento del mismo componente
+    setTimeout(() => {
+      this.mostrarRegistros = false;
+    }, 200); // Ajusta el valor según tus necesidades
   }
 
 }
