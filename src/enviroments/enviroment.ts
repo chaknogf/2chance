3a0d8d70e28e34a6815f6c7ff5c4a937d6d41651
chaknogf <chaknogf@gmail.com>
2024-03-22 14:34:06 -0600

    .

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: false,
- //apiUrl: 'http://localhost:8000'
+    // apiUrl: 'http://localhost:8000'
   // apiUrl: "http://192.88.1.191:8000"
-   apiUrl: "http://192.88.1.135:8000"
+  //  apiUrl: "http://192.88.1.135:8000"
   // apiUrl: "http://192.168.0.6:8000"
+  apiUrl: "http://192.168.0.4:8000"
+
 
 
 };
