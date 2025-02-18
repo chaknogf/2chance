b2d39e9a38f5e804c69bbbfbe09b49ec44197661
chaknogf <chaknogf@gmail.com>
2025-02-18 04:23:54 -0600

    chore: enviromen prod

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -5,6 +5,6 @@ const ip0 = 'http://' + hostname + ':8000';
 //const ip = 'http://192.168.0.200:8000';
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: ip0
 };
---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://hgtecpan.duckdns.org/api' // Usamos HTTPS y la nueva ruta del proxy
+};
